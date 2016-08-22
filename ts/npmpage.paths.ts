7e5d98410e3e0556ff88f278f3e8cd55a64c1750
Phil Kunz <github@philkunz.com>
2016-08-22 03:30:05 +0200

    small fix

--- ts/npmpage.paths.ts
+++ ts/npmpage.paths.ts
@@ -4,4 +4,3 @@ export let cwd = process.cwd();
 export let publicDir = plugins.path.join(cwd, "public/");
 export let coverageDir = plugins.path.join(cwd, "coverage/");
 export let docsDir = plugins.path.join(cwd, "docs/");
-export let 
