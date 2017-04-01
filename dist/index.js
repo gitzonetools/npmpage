d9c3ebe776cff2ba71cc4a8b944ca1d459bc6439
Phil Kunz <gitlab@philkunz.com>
2017-04-01 18:28:01 +0200

    added quicksite

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("quicksite");
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHFCQUFrQiJ9
